d5315235bc33e54a32f67f4bc1ed75ebefa38e63
Lucas <you@example.com>
2020-09-11 18:23:13 -0300

    clase2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers;
 
 class WelcomeController extends Controller {
 
+    public $y = 20;
+    public $x = 30;
+
     public function welcome()
     {
-        return view('welcome');
+        $nombre = "Pepe";
+        return view('welcome', 
+        ['name' => $nombre, 'y' => $this->y, 'x' => $this->x, 'suma' => $this->suma($this->y, $this->x)]);
     }
 
     public function getContacto()
     {
         return view('contacto');
+    }
+
+    public function suma($x, $y){
+        return $x + $y;
     }
 
 }
